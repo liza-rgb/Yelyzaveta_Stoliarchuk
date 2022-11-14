93c7a4eb242ef8e61b81527cd530d40960e3b5f5
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-11-14 19:06:36 +0100

    Updated extra JS task #1

--- extra_task1.js
+++ extra_task1.js
@@ -1,43 +1,46 @@
 const string_to_arr = (input) => {
-    return String(input).split("").map((el)=>{
-        return Number(el)
+  return String(input)
+    .split("")
+    .map((el) => {
+      return Number(el);
     });
-}
+};
 
 const swap = (index1, index2, arr) => {
-    const temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
-}
+  const temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+};
 
 const next_bigger = (num) => {
-    num_arr = stringToArr(num);
+  let num_arr = string_to_arr(num);
 
-    let i = -1;
-    for (i = num_arr.length - 1; i > -1; i--) {
-        if (num_arr[i] > num_arr[i-1]) {
-            break;
-        }
+  let i = -1;
+  for (i = num_arr.length - 1; i > -1; i--) {
+    if (num_arr[i] > num_arr[i - 1]) {
+      break;
     }
+  }
 
-    if (i === -1 ) {
-        return -1;
+  if (i === -1) {
+    return -1;
+  }
+  i--;
+
+  let j = -1;
+  for (j = num_arr.length - 1; j > i; i--) {
+    if (num_arr[i] < num_arr[j]) {
+      break;
     }
-    i--;
+  }
+  swap(i, j, num_arr);
 
-    let j = -1;
-    for (j = num_arr.length - 1; j > i; i--) {
-        if (num_arr[i] < num_arr[j]) {
-            break;
-        }
-    }
-    swap(i, j, num_arr);
+  num_arr = num_arr
+    .slice(0, i + 1)
+    .concat(num_arr.slice(i + 1, num_arr.length).reverse());
 
-    num_arr = num_arr.slice(0, i + 1)
-        .concat(num_arr.slice(i + 1, num_arr.length).reverse());
-
-    return num_arr.join('');
-}
+  return num_arr.join("");
+};
 
 console.log(`12 => ${next_bigger(12)}`);
 console.log(`513 => ${next_bigger(513)}`);
